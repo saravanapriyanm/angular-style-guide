a86b238897209b3258bcb01531205d6465f30eb1
saravanapriyanm <saravanapriyanm@gmail.com>
2021-09-17 12:49:17 +0530

    incomplete: buttons, variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GuideComponent } from './styles/guide/guide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuideComponent
   ],
   imports: [
     BrowserModule,
